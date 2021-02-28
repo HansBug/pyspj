384a7d945fb660fb0b27e9e122e3ff2e12a8b50c
HansBug <killog@126.com>
2021-03-01 00:33:17 +0800

    add test for general

--- test/models/test_general.py
+++ test/models/test_general.py
@@ -2,15 +2,34 @@ import os
 
 import pytest
 
+from pyspj.models import load_result, SimpleSPJResult, ContinuitySPJResult
+
 
 @pytest.mark.unittest
 class TestModelsGeneral:
     def test_simple(self):
-        pass
-        # load_result()
+        result = SimpleSPJResult(True, '123', '12345')
+        assert load_result(result) == result
+        assert load_result(result.to_json()) == result
+        assert load_result((True,)) == SimpleSPJResult(True, )
+        assert load_result(True) == SimpleSPJResult(True, )
+        assert load_result(None) == SimpleSPJResult(False, )
+        assert load_result((True, '123')) == SimpleSPJResult(True, '123')
+        assert load_result((True, '123', '12345')) == result
 
     def test_continuity(self):
-        pass
+        result = ContinuitySPJResult(True, 0.5, '123', '12345')
+        assert load_result(result) == result
+        assert load_result(result.to_json()) == result
+        assert load_result(((True, 0.5),)) == ContinuitySPJResult(True, 0.5)
+        assert load_result(((True, 0.5), '123')) == ContinuitySPJResult(True, 0.5, '123')
+        assert load_result(((True, 0.5), '123', '12345')) == result
+
+    def test_invalid(self):
+        with pytest.raises(ValueError):
+            assert load_result(())
+        with pytest.raises(ValueError):
+            assert load_result((1, 2, 3, 4))
 
 
 if __name__ == "__main__":
