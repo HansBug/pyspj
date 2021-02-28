5763355da25489bf7728093df52ea8b13d4645aa
HansBug <killog@126.com>
2021-02-28 23:45:13 +0800

    add test for split to lines

--- test/utils/test_string.py
+++ test/utils/test_string.py
@@ -15,8 +15,13 @@ class TestUtilsString:
                         show_length=True) == 'this is the ..(47 chars).. ind of thing'
 
     def test_split_to_lines(self):
+        assert split_to_lines('') == []
+        assert split_to_lines('\r\n') == ['']
         assert split_to_lines('this is a line\n') == [
-            'this ia a line',
+            'this is a line',
+        ]
+        assert split_to_lines('this is line 1   \r\n this is line 2 \n this is line 3\n this is line 4\r\n') == [
+            'this is line 1   ', ' this is line 2 ', ' this is line 3', ' this is line 4'
         ]
 
 
